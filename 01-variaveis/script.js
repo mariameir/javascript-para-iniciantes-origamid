7f8fd656577d8a1cb36b0e9bdb86970ce2c86706
Maria Meir Fernanda Souza Castro <pigyarx@gmail.com>
2023-07-18 15:52:20 -0400

    first commit

---
+++ 01-variaveis/script.js
@@ -0,0 +1,26 @@
+// Declarar uma variável com o seu nome
+// Declarar uma variável com a sua idade
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+// Atribuir valor a sua comida favorita
+// Declarar 5 variáveis diferentes sem valores
+
+var nome = "Maria";
+var idade = 24;
+var comida;
+comida="pão";
+
+var caneta,bebida,tamanho,jogo,time;
+
+console.log(nome);
+console.log(idade);
+console.log(comida);
+console.log(caneta);
+console.log(bebida);
+console.log(tamanho);
+console.log(jogo);
+console.log(time);
+
+
+
+
---
+++ 02-tipos-de-dados/script.js
@@ -0,0 +1,23 @@
+// Declare uma variável contendo uma string
+
+// Declare uma variável contendo um número dentro de uma string
+
+// Declare uma variável com a sua idade
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+// Coloque a seguinte frase em uma variável: It's time
+
+// Verifique o tipo da variável que contém o seu nome
+
+frase = "SEPtt";
+console.log(typeof frase);
+
+numeroString= "12";
+console.log(typeof numeroString);
+
+idade= 24;
+
+nome= "maria";
+sobrenome= "souza";
