aa29ecbc7e3ec47af74c768d2b7d62a9b49ef676
Maria Meir Fernanda Souza Castro <pigyarx@gmail.com>
2023-07-19 08:42:22 -0400

    numeros e operadores 2

--- 03-numeros-e-operadores/script.js
+++ 03-numeros-e-operadores/script.js
@@ -1,3 +1,23 @@
-var x = 5;
-console.log(x--);
-console.log(x)
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+//35
+
+// Crie duas expressões que retornem NaN
+var calculo = 'verde';
+console.log(calculo/2);
+console.log(calculo*2);
+
+// Somar a string '200' com o número 50 e retornar 250
+var num1 = +'200';
+var num2 = 50;
+console.log(num1+num2)
+// Incremente o número 5 e retorne o seu valor incrementado
+num3  = 5;
+console.log(++num3);
+
+// Como dividir o peso por 2?
+numero = +'80'/2;
+unidade = 'kg';
+peso = numero + unidade; // '80kg'
+pesoPorDois = peso; // NaN (Not a Number)
+console.log(pesoPorDois)
