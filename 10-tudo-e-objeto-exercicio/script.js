719a8afca4f5da5a6962fdad40ddf7ee9ee8ffc3
Maria Meir <pigyarx@gmail.com>
2023-07-26 09:31:11 -0400

    resolução do exdercicio

--- 10-tudo-e-objeto-exercicio/script.js
+++ 10-tudo-e-objeto-exercicio/script.js
@@ -1,6 +1,29 @@
 // nomeie 3 propriedades ou métodos de strings
+var nome = 'Meir';
+nome.toUpperCase();
+nome.toLocaleLowerCase();
+nome.length;
 
 // nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+btn.classList.add('verdao');
+btn.innerHTML;//clique
+btn.addEventListener('click',function(){
+  console.log('iiih')
+})
+
+
 
 // busque na web um objeto (método) capaz de interagir com o clipboard, 
 // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+
+  
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(copyText.value);
+
+  alert("Copied the text: " + copyText.value);
+}
