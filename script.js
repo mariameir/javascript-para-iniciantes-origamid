8b82554cfef24fdc9fa21a329e8738f976e8805e
Maria Meir <pigyarx@gmail.com>
2023-11-16 14:59:46 -0400

    estudos

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// Qual o resultado da seguinte expressão?
-var total = 10 + 5 * 2 / 2 + 20;
 
+var corfav = "red"
 
-// Crie duas expressões que retornem NaN
-
-// Somar a string '200' com o número 50 e retornar 250
-
-// Incremente o número 5 e retorne o seu valor incrementado
-
-// Como dividir o peso por 2?
-var numero = '80';
-var unidade = 'kg';
-var peso = numero + unidade; // '80kg'
-var pesoPorDois = peso / 2; // NaN (Not a Number)
+switch (corfav) {
+  case 'Vermelho':
+    console.log("vermelho")
+    break;
+  case 'Azul':
+    console.log("azul")
+    break;
+  default:
+    console.log("nenhuma")
+}
